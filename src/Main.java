15e2fd7325249cfedd3a53aa7e7069fccd86d0f6
Adam8J <adam.jurasz@wp.pl>
2022-10-26 19:35:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rabbit name1 = new Rabbit(15, "Hony", true );
+
+    }
+}
---
+++ src/Rabbit.java
@@ -0,0 +1,11 @@
+public class Rabbit {
+    int lenght;
+    String name;
+    boolean softness;
+
+    Rabbit (int length, String name,boolean softness){
+        this.lenght = length;
+        this.name = name;
+        this.softness = softness;
+    }
+}
